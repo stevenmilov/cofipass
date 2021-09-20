af495b1b4b4bdef0acd25f695f1501b90399648e
Steven Milov <steven@mavenmachines.com>
2021-09-20 12:55:03 -1000

    first commit!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import AppLayout from "./components/AppLayout";
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+
+const AppLayout = () => {
+  return (
+    <div className="app-layout">
+      <div className="header">COFIPASS</div>
+      <div className="body">This is some smaller text that goes here</div>
+    </div>
+  );
+};
+
+export default AppLayout;
